6319c411d0788c212522c760a72f9359e7b6e8b0
kamalyahyha <kamalyahya284@gmail.com>
2025-02-13 21:06:33 +0300

    sf

--- scripts/c3main.js
+++ scripts/c3main.js
@@ -1,4 +1,4 @@
-import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/box2d.wasm.js";
-import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/scripts/c3runtime.js";
-import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/scripts/objRefTable.js";
-import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/scripts/project/instant-games-bridge.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/lol@main/box2d.wasm.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/lol@main/scripts/c3runtime.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/lol@main/scripts/objRefTable.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/lol@main/scripts/project/instant-games-bridge.js";
