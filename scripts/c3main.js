46a747ec9898fa5f15b913a67e5854a52a7a45ed
kamalyahyha <kamalyahya284@gmail.com>
2025-02-13 21:04:06 +0300

    sfsf

---
+++ scripts/c3main.js
@@ -0,0 +1,4 @@
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/box2d.wasm.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/scripts/c3runtime.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/scripts/objRefTable.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/scripts/project/instant-games-bridge.js";
